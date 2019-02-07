455aaefc1e556dd0d1da87c9d6287ebf4a6618c8
Admin <admin@mac-admin.local>
2019-02-07 19:23:05 +0300

    update

--- ios-itodolist/ViewController.swift
+++ ios-itodolist/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "MainVC") as! MainViewController
+        self.present(vc, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
