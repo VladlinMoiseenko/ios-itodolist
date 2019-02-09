40a4248751c181c388b655128bd74b8454467348
Admin <admin@mac-admin.local>
2019-02-09 16:12:48 +0300

    update

--- ios-itodolist/View/LoginViewController.swift
+++ ios-itodolist/View/LoginViewController.swift
@@ -14,15 +14,12 @@ import SwinjectStoryboard
 
 class ViewController: UIViewController {
 
-    @IBAction func buttonClicked(_ sender: UIButton) {
-        let vc = storyboard?.instantiateViewController(withIdentifier: "MainVC") as! MainViewController
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-    
-
+    @IBOutlet var _username: UITextField!
+    @IBOutlet var _password: UITextField!
+    @IBOutlet var _login_button: UIButton!
     
     var loginViewModel: LoginViewModel?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         initInjections()
@@ -30,6 +27,42 @@ class ViewController: UIViewController {
         //loginViewModel?.fetchFirstData()
     }
 
+    @IBAction func LoginButton(_ sender: Any) {
+        
+        let username = _username.text
+        let password = _password.text
+        
+        if (username == "") {
+            let alert = UIAlertController(title: "Username is empty", message: "Please type username.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+
+        if (password == "") {
+            let alert = UIAlertController(title: "Password is empty", message: "Please type password.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        doLogin(username!, password!)
+        
+    }
+    
+    func doLogin(_ user:String, _ pass:String) {
+        // authorize REST
+        
+        
+        
+        
+        
+//        let vc = storyboard?.instantiateViewController(withIdentifier: "MainVC") as! MainViewController
+//        self.navigationController?.pushViewController(vc, animated: true)
+
+    }
+    
+    
     private func bindObservables() {
         //_ = loginViewModel?.title.asObservable().bind(to: tLabel.rx.text)
     }
